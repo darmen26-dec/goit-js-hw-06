0e66c5c6d9a175082b5f6a479a554611175ea310
darmen26-dec <dariamendelewska@gmail.com>
2023-08-31 23:50:49 +0200

    fixes

--- js/task-02.js
+++ js/task-02.js
@@ -18,10 +18,40 @@ const ingredients = [
 
 const ingredientsList = document.querySelector('#ingredients');
 
-ingredients.forEach((ingredient) => {
+const listItems = ingredients.map((ingredient) => {
   const listItem = document.createElement('li');
   listItem.textContent = ingredient;
-
   listItem.classList.add('item');
-  ingredientsList.append(listItem);
+  return listItem;
 });
+
+ingredientsList.append(...listItems);
+
+// Pierwsze rozwiązanie
+
+// 'use strict';
+
+// const ingredients = [
+//   'Potatoes',
+//   'Mushrooms',
+//   'Garlic',
+//   'Tomatos',
+//   'Herbs',
+//   'Condiments',
+// ];
+
+// const ingredientsList = document.querySelector('#ingredients');
+
+// ingredients.forEach((ingredient) => {
+//   const listItem = document.createElement('li');
+//   listItem.textContent = ingredient;
+
+//   listItem.classList.add('item');
+//   ingredientsList.append(listItem);
+// });
+
+// Efekt rozwiązania zadania jest właściwy jednak poproszę Cię o zmianę. Zwróc uwagę, że w Twoim rozwiązaniu iterujesz
+// po tablicy, tworzysz elementy i dodajesz kolejno do ingredientsList. Nie jest to efektywne, pomyśl gdyby tych elementów
+// było np. 100, 1000.... Lepiej jest je wygenerowac i dodać wszystkie na końcu do listy.
+// Wykorzystaj w zadaniu metode tablicową map(), zmien tylko sposób dodawania item do listy tj. dodaj poza metodą
+// wszystkie  elementy - podobnie jak w zadaniu 3.
